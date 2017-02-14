fa62984fc01b2f5cbfaf2d892c373df4ee6c2005
Vipul Bhale <vipulbhale@gmail.com>
2017-02-13 21:19:05 -0500

    Second Commit

--- src/main/java/aug/manas/splitter/config/WebConfig.java
+++ src/main/java/aug/manas/splitter/config/WebConfig.java
@@ -2,14 +2,13 @@ package aug.manas.splitter.config;
 
 import org.springframework.context.annotation.Bean;
 
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
